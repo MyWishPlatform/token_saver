d637709c55f3d1ac624c81df0cca3d6a2273cf5e
wadguk <57893892+wadguk@users.noreply.github.com>
2019-12-19 16:26:27 +0300

    Add files via upload

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,28 @@
+const TokenSaver = artifacts.require("./contracts/TokenSaver.sol");
+const ERC20 = artifacts.require("./ERC20.sol");
+const timeHelper = require('../test/utils/utils.js');
+
+(async function () {
+    await timeHelper.revertToSnapShot('0x1');                        //Revert local ganache time
+    console.log("Main Address:", await getAccount(0));
+    console.log("Reserve Address:", await getAccount(1));
+})()
+
+const getAccount = async (i) => {
+    try {
+        const myAccounts = await web3.eth.getAccounts();
+        return myAccounts[i];
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = async function (deployer) {
+    deployer.deploy(TokenSaver);
+    console.log('TIME NOW:', Math.floor(Date.now() / 1000) + 144000);
+    console.log('TIME NOW:', now = new Date());
+    deployer.deploy(ERC20, 10000);
+
+};
+
+
