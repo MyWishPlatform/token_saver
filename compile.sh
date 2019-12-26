58803a4c70a0f83a6986f41a6cd84315c26f9a0b
wadguk <57893892+wadguk@users.noreply.github.com>
2019-12-26 15:35:13 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 c-preprocessor --config c-preprocessor-config.json TemplateForTest.sol TokenSaverTest.sol
 c-preprocessor --config c-preprocessor-config.json TemplateForDeploy.sol TokenSaver.sol
+rm -rf `pwd`/contracts/TokenSaverTest.sol
+rm -rf `pwd`/contracts/TokenSaver.sol
 mv TokenSaverTest.sol `pwd`/contracts
 mv TokenSaver.sol `pwd`/contracts
 rm -rf `pwd`/build/contracts/TokenSaverTest.json
