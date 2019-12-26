f43da455b85398ab01e015446095da07ec4c34b8
wadguk <57893892+wadguk@users.noreply.github.com>
2019-12-26 15:50:03 +0300

    Update test-compiled.sh

--- test-compiled.sh
+++ test-compiled.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 c-preprocessor --config c-preprocessor-config.json TestTemplate.js mainTest.js
+ganache-cli &
 mv mainTest.js `pwd`/test
 truffle test
+lsof -i :8545 -sTCP:LISTEN |awk 'NR > 1 {print $2}'  |xargs kill -15
