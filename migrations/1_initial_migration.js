eb22e2cd72a427cec02c97af6cd687bbfcf9be28
wadguk <57893892+wadguk@users.noreply.github.com>
2019-12-26 15:29:08 +0300

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,11 +1,9 @@
-const TokenSaver = artifacts.require("./contracts/TokenSaver.sol");
+const TokenSaverTest = artifacts.require("./contracts/TokenSaverTest.sol");
 const ERC20 = artifacts.require("./ERC20.sol");
 const timeHelper = require('../test/utils/utils.js');
 
 (async function () {
-    await timeHelper.revertToSnapShot('0x1');                        //Revert local ganache time
-    console.log("Main Address:", await getAccount(0));
-    console.log("Reserve Address:", await getAccount(1));
+    await timeHelper.revertToSnapShot('0x1');                           //revert time
 })()
 
 const getAccount = async (i) => {
@@ -18,11 +16,10 @@ const getAccount = async (i) => {
 }
 
 module.exports = async function (deployer) {
-    deployer.deploy(TokenSaver);
-    console.log('TIME NOW:', Math.floor(Date.now() / 1000) + 144000);
+    deployer.deploy(TokenSaverTest, getAccount(0), getAccount(1),
+        Math.floor(Date.now() / 1000) + 150000);                         // Data shift
     console.log('TIME NOW:', now = new Date());
     deployer.deploy(ERC20, 10000);
-
 };
 
 
