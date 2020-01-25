67921d63dc6f1780ed70a91bb85386e7fab8a4ac
ilya <ilya@ilyas-MacBook-Air.local>
2020-01-26 01:18:23 +0300

    coverage port

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // port: 6545,
-  // testrpcOptions: '-p 6545 -u 0x54fd80d6ae7584d8e9a19fe1df43f04e5282cc43',
+  port: 4545
+  // testrpcOptions: "-p 4545"
   // testCommand: 'truffle test',
   // norpc: true,
   // dir: './secretDirectory',
