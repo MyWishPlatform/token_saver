7b620befa03b38a51810713792d7c299c9f3baed
wadguk <57893892+wadguk@users.noreply.github.com>
2020-04-08 17:18:07 +0300

    Oracle execution modifier added

--- TokenSaver(ORIGINAL).sol
+++ TokenSaver(ORIGINAL).sol
@@ -1,14 +1,13 @@
- pragma solidity ^ 0.5.12;
+ pragma solidity  ^0.5.12;
 
-// ERC20 interface.
-contract ERC20Token {
-    function balanceOf(address) public view returns(uint);
-    function allowance(address, address) public view returns(uint);
-    function transfer(address, uint) public returns(bool);
-    function approve(address, uint)  public returns(bool);
-    function transferFrom(address, address, uint) public returns(bool);
+    // ERC20 interface.
+interface ERC20Token {
+    function balanceOf(address) external view returns(uint);
+    function allowance(address, address) external view returns(uint);
+    function transfer(address, uint) external returns(bool);
+    function approve(address, uint)  external returns(bool);
+    function transferFrom(address, address, uint) external returns(bool);
 }
-
 
 contract TokenSaver {
     // Protected address.
@@ -24,32 +23,37 @@ contract TokenSaver {
     uint public endTimestamp;
 
     // Oracle address.
-    address constant public oracleAddress;
+    address public oracleAddress;
 
     // Oracle option.
-     bool constant public oracleEnabled;
+    bool public oracleEnabled;
 
     //Oracle time stamp interval
-    uint constant public timestampInterval;
+    uint public timestampInterval;
 
     // Array with token addresses.
     address[] public tokenType;
 
-    // Modifier allows execution only from owner address.
+    // Get information about the current execution context, including the sender of the transaction
+    function msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+    // Modifier allows execution only from owner or oracle address.
     modifier onlyOwner(){
-        require(msg.sender == owner);
+        require(msgSender() == owner || msgSender() == oracleAddress);
         _;
     }
     
     // Modifier allows execution only from backend address.
     modifier onlyBackend(){
-        require(msg.sender == backendAddress);
+        require(msgSender() == backendAddress);
         _;
     }
     
     /**
-     * Event for safed tokens logging.
-     * @param tokenToSave safed token address.
+     * Event for saved tokens logging.
+     * @param tokenToSave saved token address.
      */
     event TokensToSave(address tokenToSave);
     
@@ -62,11 +66,11 @@ contract TokenSaver {
     /**
      * Event for transactions logging.
      * @param tokenType token address.
-     * @param succeededAmount amount of safed tokens.
+     * @param succeededAmount amount of saved tokens.
      */
     event TransactionInfo(address tokenType, uint succeededAmount);
 
-    constructor(address _ownerAddress, address _reserveAddress, uint _endTimestamp) public {
+    constructor(address _ownerAddress, address _reserveAddress, uint _endTimestamp , address _oracleAddress) public {
         require(_ownerAddress != address(0),"Invalid OWNER address");
         require(_reserveAddress != address(0),"Invalid RESERVE address");
         require(_endTimestamp > now, "Invalid TIMESTAMP");
@@ -74,11 +78,13 @@ contract TokenSaver {
         backendAddress = msg.sender;
         reserveAddress = _reserveAddress;
         endTimestamp = _endTimestamp;
+        oracleAddress = _oracleAddress;
     }
 
     /**
-     * Add tokens to safe.
-     * @param _tokenAddressArray token addresses to safe in array format.
+     * Add tokens to the safe.
+     * @param _tokenAddressArray token addresses to save in array format.
+     * @return bool whether the given operation is succeed 
      */
     function addTokenType(address[] memory _tokenAddressArray) public onlyBackend returns(bool) {
         require(_tokenAddressArray[0] != address(0), "Invalid address");
@@ -96,16 +102,17 @@ contract TokenSaver {
     }
 
     /**
-     * Get owner balance at specified token address.
+     * Get owner balance at the specified token address.
      * @param _tokenAddress token address.
      * @param _owner owner address.
+     * @return uint balance of the given address at the given token instance
      */
     function getBalance(address _tokenAddress, address _owner) private view returns(uint){
         return ERC20Token(_tokenAddress).balanceOf(_owner);
     }
 
     /**
-     * @dev Call this function to verify ERC20 interface and owner participation.
+     * @dev Call this function to verify the ERC20 interface and owner participation.
      * @param _tokenAddress token address to verify.
      */
     function tryGetResponse(address _tokenAddress) private returns(bool) {
@@ -118,16 +125,16 @@ contract TokenSaver {
 
     /**
      * Get allowed amount to spend.
-     * @param _tokenAddress token address to check allowed amount.
+     * @param _tokenAddress token address to check the allowed amount.
      */
     function getAllowance(address _tokenAddress) private view returns(uint){
         return ERC20Token(_tokenAddress).allowance(owner, address(this));
     }
 
     /**
-     * Transfer tokens from protected address to reserve address.
+     * Transfer tokens from the protected address to the reserve address.
      * @param _tokenAddress token address.
-     * @param _amount amount to transfer.
+     * @param _amount uint amount to transfer.
      */
     function transferFromOwner(address _tokenAddress, uint _amount) private returns(bool){
         ERC20Token(_tokenAddress).transferFrom(owner, reserveAddress, _amount);
@@ -135,11 +142,11 @@ contract TokenSaver {
     }
 
     /**
-     * Fallback function to execute token transfer.
+     * Fallback function to execute the token transfer.
      * @dev execution time must be correct.
      */
     function() external {
-        require((!oracleEnabled && now > endTimestamp) || (oracleEnabled && msg.sender == oracleAddress), "Invalid verify unlock");
+        require((!oracleEnabled && now > endTimestamp) || (oracleEnabled && msgSender() == oracleAddress), "Invalid verify unlock");
         uint balance;
         uint allowed;
         uint balanceContract;
@@ -171,7 +178,8 @@ contract TokenSaver {
         }
     }
 
-    // Self-destruction.
+    // Self-destruction. 
+    // Execution allowed only from owner or oracle address
     function selfdestruction() public onlyOwner{
         emit SelfdestructionEvent(true);
         selfdestruct(address(0));
